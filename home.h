f39caa5102ab1a07b282210637e3d879a6b25d4b
ahmed <ahmed@AHMED-WAHDAN>
2024-04-04 16:51:07 +0200

    fixed project settings

---
+++ home.h
@@ -0,0 +1,31 @@
+#ifndef HOME_H
+#define HOME_H
+#include"opengl_graph.h"
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Home;
+}
+QT_END_NAMESPACE
+
+class Home : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Home(QWidget *parent = nullptr);
+    ~Home();
+
+private:
+    Ui::Home *ui;
+    opengl_graph* graph = new opengl_graph(this);
+
+public slots:
+    void btn_1_handle();
+    void btn_1_handle2();
+    void ADDnode();
+
+
+};
+#endif // HOME_H
